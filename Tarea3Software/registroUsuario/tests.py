14974667fc1885571bfb0869b52aa091a1403b40
julio <perezduranjulio@gmail.com>
2018-10-19 07:50:53 -0400

    Fallo en prueba, falta refactorizar

--- Tarea3Software/registroUsuario/tests.py
+++ Tarea3Software/registroUsuario/tests.py
@@ -7,13 +7,28 @@ class FunctionsTester(TestCase):
     
     # primera prueba de test
     def testFirstRegister(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Olabraa2", "Olabraa2"))
+        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "OOlabraa23", "OOlabraa23"))
     
     #frontera: tiene 0 digitos la contrasena
-    def testDigitsPassword(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "Olabraa", "Olabraa"))
+    def testZeroDigitsPassword(self):
+        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "OOlabraa", "OOlabraa"))
     
-    #frontera: clave no valida
+    #frontera: clave no valida falta .
     def testNotValidPassword(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@", "Olabraa2", "Olabraa2"))
+        self.assertFalse(VerificarRegistro("pepitogonzales@", "OOlabraa2", "OOlabraa2"))
+        
+    #frontera: clave tiene 1 digito
+    def testOneDigitValidPassword(self):
+        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "OOlabraa2", "OOlabraa2"))
+        
+    #frontera: clave tiene 2 letras
+    def testTwoLettersPassword(self):
+        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "Ol22222", "Ol22222"))
+    
+    #frontera: clave tiene 3 letras
+    def testThreeLetterPassword(self):
+        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Oll222222", "Oll222222"))
+    
+    
+    
     
