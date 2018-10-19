d70a5e7666b4a4dd518e9946dccb891a0d34c47b
julio <perezduranjulio@gmail.com>
2018-10-18 20:28:58 -0400

    Se añadio primer caso de prueba: resultado esperado True pero todavia no existe la funcion VerificarRegistro()

---
+++ Tarea3Software/registroUsuario/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+# Create your tests here.
+def testAgeNegativeMan(self):
+    self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Olabraa2", "Olabraa2"))
