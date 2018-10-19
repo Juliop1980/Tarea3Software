a2ee43cf145507a427fdf0e921e0f3d2c81ad992
julio <perezduranjulio@gmail.com>
2018-10-18 23:06:02 -0400

    Se agrego funcion VerificarRegistro, Exitosa

---
+++ Tarea3Software/registroUsuario/seguridad.py
@@ -0,0 +1,40 @@
+import re
+
+def VerificarRegistro(correo, clave1, clave2):
+    return VerificarClavesCoinciden(clave1, clave2) and ValidarCorreo(correo) and ValidarClave(clave1)
+
+def ValidarClave(clave):
+    return ValidarLongitudClave(clave) and ValidarNoCaracteresEspeciales(clave) and ContieneAlMenosTresLetras(clave) and ContieneAlMenosUnaMayuscula(clave)  and ContieneAlMenosUnaMinuscula(clave) and ContieneAlMenosUnDigito(clave)
+      
+def VerificarClavesCoinciden(clave1, clave2):
+    return clave1==clave2
+
+def ValidarCorreo(correo):
+    patron = "^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$"
+    Correo_REGEX = re.compile(patron)
+    result = bool(Correo_REGEX.match(correo))
+    return result
+    
+def ValidarLongitudClave(clave):
+    longitudMinimaClave = 8
+    longitudMaximaClave = 16
+    longitudCorreo = len(clave)
+    return longitudCorreo >= longitudMinimaClave and longitudCorreo <= longitudMaximaClave
+
+def ValidarNoCaracteresEspeciales(clave):
+    return bool(re.match("^[a-zA-Z0-9_]*$", clave))
+
+def ContieneAlMenosTresLetras(clave):
+    return bool(re.match("(.*[a-z]){3}", clave))
+
+def ContieneAlMenosUnaMinuscula(clave):
+    return any(x.islower() for x in clave)
+
+def ContieneAlMenosUnaMayuscula(clave):
+    return any(x.isupper() for x in clave)
+
+def ContieneAlMenosUnDigito(clave):
+    return any(x.isdigit() for x in clave)
+    
+
+
--- Tarea3Software/registroUsuario/tests.py
+++ Tarea3Software/registroUsuario/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from registroUsuario.seguridad import VerificarRegistro
 
 # Create your tests here.
-def testAgeNegativeMan(self):
-    self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Olabraa2", "Olabraa2"))
+class FunctionsTester(TestCase):
+    
+    def testFirstRegister(self):
+        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Olabraa2", "Olabraa2"))
