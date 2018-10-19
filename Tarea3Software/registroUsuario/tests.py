37599c7e636612cf612eaa5459852dea867b83fd
julio <perezduranjulio@gmail.com>
2018-10-19 00:13:21 -0400

    Agregada prueba frontera exitosa

--- Tarea3Software/registroUsuario/tests.py
+++ Tarea3Software/registroUsuario/tests.py
@@ -4,5 +4,13 @@ from registroUsuario.seguridad import VerificarRegistro
 # Create your tests here.
 class FunctionsTester(TestCase):
     
+    
+    # primera prueba de test
     def testFirstRegister(self):
         self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Olabraa2", "Olabraa2"))
+    
+    #frontera: tiene 0 digitos la contrasena
+    def testDigitsPassword(self):
+        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "Olabraa", "Olabraa"))
+        
+    
