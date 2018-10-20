50db16bb2a1a3dfd44d2b9ff3c48c8336411bfdf
Julio Cesar Perez Duran <perezduranjulio@gmail.com>
2018-10-19 21:01:18 -0400

    agregadas pruebas, funciones refactorizadas

--- Tarea3Software/registroUsuario/seguridad.py
+++ Tarea3Software/registroUsuario/seguridad.py
@@ -1,45 +1,72 @@
 import re
 
-def VerificarRegistro(correo, clave1, clave2):
-    return VerificarClavesCoinciden(clave1, clave2) and ValidarCorreo(correo) and ValidarClave(clave1)
 
-def ValidarClave(clave):
-    return ValidarLongitudClave(clave) and ValidarNoCaracteresEspeciales(clave) and ContieneAlMenosTresLetras(clave) and ContieneAlMenosUnaMayuscula(clave)  and ContieneAlMenosUnaMinuscula(clave) and ContieneAlMenosUnDigito(clave)
-      
-def VerificarClavesCoinciden(clave1, clave2):
-    return clave1==clave2
+class Seguridad:
 
-def ValidarCorreo(correo):
-    patron = "[^@]+@[^@]+\.[^@]+"
-    Correo_REGEX = re.compile(patron)
-    result = bool(Correo_REGEX.match(correo))
-    return result
-    
-def ValidarLongitudClave(clave):
-    longitudMinimaClave = 8
-    longitudMaximaClave = 16
-    longitudCorreo = len(clave)
-    return longitudCorreo >= longitudMinimaClave and longitudCorreo <= longitudMaximaClave
+    def __init__(self):
+        self.my_dict = {}
 
-def ValidarNoCaracteresEspeciales(clave):
-    return bool(re.match("^[a-zA-Z0-9_]*$", clave))
+    def registrarUsuario(self,correo, clave1, clave2):
+        resultado = self.VerificarClavesCoinciden(clave1, clave2) and self.ValidarCorreo(correo) and self.ValidarClave(clave1)
+        clave_dict = clave1[::-1]
+        if resultado == True:
+            self.my_dict[correo] = clave_dict
+        return resultado
 
-def ContieneAlMenosTresLetras(clave):
-    return bool(re.match("(.*[a-zA-Z]){3}", clave))
+    def ValidarClave(self,clave):
+        validacion1 = self.ValidarLongitudClave(clave) 
+        validacion2 = self.ValidarNoCaracteresEspeciales(clave)
+        validacion3 = self.ContieneAlMenosTresLetras(clave)
+        validacion4 = self.ContieneAlMenosUnaMayuscula(clave) 
+        validacion5 = self.ContieneAlMenosUnaMinuscula(clave)
+        validacion6 = self.ContieneAlMenosUnDigito(clave)
+        resultado = validacion1 and validacion2 and validacion3 and validacion4 and validacion5 and validacion6
+        if resultado == False:
+            mensaje = "Clave Invalida"
+            if !validacion1:
+                mensaje = mensaje + ", longitud de clave incorrecta"
 
-def ContieneAlMenosUnaMinuscula(clave):
-    return any(x.islower() for x in clave)
+            if !validacion2:
+                mensaje = mensaje + ", posee caracteres especiales"
 
-def ContieneAlMenosUnaMayuscula(clave):
-    return any(x.isupper() for x in clave)
+        return resultado
+          
+    def VerificarClavesCoinciden(self,clave1, clave2):
+        return clave1==clave2
 
-def ContieneAlMenosUnDigito(clave):
-    return any(x.isdigit() for x in clave)
+    def ValidarCorreo(self,correo):
+        patron = "[^@]+@[^@]+\.[^@]+"
+        Correo_REGEX = re.compile(patron)
+        result = bool(Correo_REGEX.match(correo))
+        if result == False:
+            print("Correo electrónico inválido")
+        return result
+        
+    def ValidarLongitudClave(self,clave):
+        longitudMinimaClave = 8
+        longitudMaximaClave = 16
+        longitudCorreo = len(clave)
+        return longitudCorreo >= longitudMinimaClave and longitudCorreo <= longitudMaximaClave
 
-def IngresarUsuario(correo, clave, my_dict):
-    clave_dict = clave[::-1]
-    element = my_dict.get(correo)
-    return element == clave_dict
+    def ValidarNoCaracteresEspeciales(self, clave):
+        return bool(re.match("^[a-zA-Z0-9_]*$", clave))
+
+    def ContieneAlMenosTresLetras(self,clave):
+        return bool(re.match("(.*[a-zA-Z]){3}", clave))
+
+    def ContieneAlMenosUnaMinuscula(self,clave):
+        return any(x.islower() for x in clave)
+
+    def ContieneAlMenosUnaMayuscula(self,clave):
+        return any(x.isupper() for x in clave)
+
+    def ContieneAlMenosUnDigito(self,clave):
+        return any(x.isdigit() for x in clave)
+
+    def IngresarUsuario(self, correo, clave):
+        clave_dict = clave[::-1]
+        element = self.my_dict.get(correo)
+        return element == clave_dict
 
     
     
--- Tarea3Software/registroUsuario/tests.py
+++ Tarea3Software/registroUsuario/tests.py
@@ -1,107 +1,110 @@
 from django.test import TestCase
-from registroUsuario.seguridad import VerificarRegistro, IngresarUsuario
+from registroUsuario.seguridad import Seguridad
 
 # Create your tests here.
-class VerificarRegistroTester(TestCase):
-    
+class registrarUsuarioTester(TestCase):
+
+    def setUp(self):
+        self.clasePrueba = Seguridad()
     
     # primera prueba de test
     def testFirstRegister(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "OOlabraa23", "OOlabraa23"))
+        self.assertTrue(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "OOlabraa23", "OOlabraa23"))
 
     #prueba de borde: claves no coinciden
     def testNoMatchingPasswords(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "OOlabraaaaa23", "OOlabraaa223"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "OOlabraaaaa23", "OOlabraaa223"))
     
     #Borde Falla al tener un caracter especial
     def testCaracteresEspeciales(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "hola-chao2H", "hola-chao2H"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "hola-chao2H", "hola-chao2H"))
         
     #Borde:email no valido
     def testNotValidEmail(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@", "OOlabraa2", "OOlabraa2"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@", "OOlabraa2", "OOlabraa2"))
     
     #frontera: tiene 0 digitos la contrasena
     def testZeroDigitsPassword(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "OOlabraaa", "OOlabraaa"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "OOlabraaa", "OOlabraaa"))
         
     #frontera: clave tiene 1 digito
     def testOneDigitValidPassword(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "OOlabraa2", "OOlabraa2"))
+        self.assertTrue(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "OOlabraa2", "OOlabraa2"))
     
     #frontera: clave tiene 3 letras
     def testThreeLetterPassword(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Oll222222", "Oll222222"))
+        self.assertTrue(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "Oll222222", "Oll222222"))
         
     #frontera: clave tiene 0 mayusculas
     def testZeroUpperCaseLettersPassword(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "sssll222222", "sssll222222"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "sssll222222", "sssll222222"))
     
     #frontera: clave tiene 0 minusculas
     def testZeroLowerCaseLettersPassword(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "AAAAA222222", "AAAAA222222"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "AAAAA222222", "AAAAA222222"))
     
     #frontera: clave tiene 8 caracteres
     def testHasLengthEight(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "ssAsOO23", "ssAsOO23"))
+        self.assertTrue(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "ssAsOO23", "ssAsOO23"))
         
     #frontera: clave tiene 16 caracteres
     def testHasLengthSixteen(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "ssAsOO23ssAsOO23", "ssAsOO23ssAsOO23"))
+        self.assertTrue(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "ssAsOO23ssAsOO23", "ssAsOO23ssAsOO23"))
     
     # frontera: Falla al tener longitud 7
     def testDebajoMin(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "Abcd123", "Abcd123"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "Abcd123", "Abcd123"))
 
     # frontera: Falla al tener longitud 17
     def testEncimaMax(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "Abcdefgh123456789", "Abcdefgh123456789"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "Abcdefgh123456789", "Abcdefgh123456789"))
     
     #esquina: clave tiene 2 letras, una mayus y una minus
     def testTwoLettersPassword(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "Ol22222", "Ol22222"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "Ol22222", "Ol22222"))
     
     # esquina: Esquina tiene 1 mayus y 3 letras
     def testHasOneUpperLetterAndThreeLetters(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Abc123124", "Abc123124"))
+        self.assertTrue(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "Abc123124", "Abc123124"))
     
     # esquina: Email invalido y contrasena invalida
     def testInvalidEmailNotMatchingPassword(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmailcom", "Abc12312", "Abc123124"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmailcom", "Abc12312", "Abc123124"))
         
     # esquina: Esquina tiene 0 mayus y 2 letras
     def testHasZeroUpperLetterAndTwoLetters(self):
-        self.assertFalse(VerificarRegistro("pepitogonzales@gmail.com", "bc123124", "bc123124"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "bc123124", "bc123124"))
     
     # esquina: Esquina tiene 1 digito y una mayus
     def testOneDigitAndOneUpperCase(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "Abcbcsdgf1", "Abcbcsdgf1"))
+        self.assertTrue(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "Abcbcsdgf1", "Abcbcsdgf1"))
     
     # esquina: Esquina tiene 1 digito y  una minus
     def testOneDigitAndOneLowerCase(self):
-        self.assertTrue(VerificarRegistro("pepitogonzales@gmail.com", "AAAAAAb1", "AAAAAAb1"))
+        self.assertTrue(self.clasePrueba.registrarUsuario("pepitogonzales@gmail.com", "AAAAAAb1", "AAAAAAb1"))
         
     # malicia: colocan solo los caracteres especiales del email
     def testSoloCaracteresEspecialesEmail(self):
-        self.assertFalse(VerificarRegistro("@.", "AAAAAAb1", "AAAAAAb1"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("@.", "AAAAAAb1", "AAAAAAb1"))
         
     # malicia: colocan solo .com
     def testSoloCom(self):
-        self.assertFalse(VerificarRegistro("@.com", "AAAAAAb1", "AAAAAAb1"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("@.com", "AAAAAAb1", "AAAAAAb1"))
         
     # malicia: colocan email al reves
     def testReverseEmail(self):
-        self.assertFalse(VerificarRegistro("moc.liamg@selaz", "AAAAAAb1", "AAAAAAb1"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("moc.liamg@selaz", "AAAAAAb1", "AAAAAAb1"))
         
     # malicia: la segunda clave es el reverso de la clave 2
     def testReversePassword(self):
-        self.assertFalse(VerificarRegistro("moc.liamg@selaz", "AAAAAAb1", "1bAAAAAA"))
+        self.assertFalse(self.clasePrueba.registrarUsuario("moc.liamg@selaz", "AAAAAAb1", "1bAAAAAA"))
         
     
 class IngresarUsuarioTester(TestCase):
-    
+
     def setUp(self):
-        self.my_dict =    {
+        self.clasePrueba = Seguridad()
+        self.clasePrueba.my_dict =    {
               "pepitogonzales@gmail.com" : "32aarbalOO",
               "pepitomater@gmail.com" :"2aarbalOO",
               "andreacolliani@hotmail.com": "432rereeA"
@@ -109,7 +112,15 @@ class IngresarUsuarioTester(TestCase):
     
     # primera prueba de ingreso
     def testPrimerIngreso(self):
-        self.assertTrue(IngresarUsuario("pepitogonzales@gmail.com", "OOlabraa23", self.my_dict))
+        self.assertTrue(self.clasePrueba.IngresarUsuario("pepitogonzales@gmail.com", "OOlabraa23"))
+
+    # borde:positivo
+    def testIngresoPositivo(self):
+        self.assertTrue(self.clasePrueba.IngresarUsuario("pepitomater@gmail.com" ,"OOlabraa2"))
+
+    # borde:negativo
+    def testIngresoNegativo(self):
+        self.assertFalse(self.clasePrueba.IngresarUsuario("pepitogonzales@gmail.com", "OOlabraa"))
     
     
     
